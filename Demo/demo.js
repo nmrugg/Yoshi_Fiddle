c1f7757db8aac4f9cf4dfe1e16fc435d021c5b65
user <user@ruggfamily.com>
2011-12-09 01:35:55 +0800

    Now the Yoshi has simple artificial intelligence.

--- Demo/demo.js
+++ Demo/demo.js
@@ -32,9 +32,12 @@ $(document).ready(function() {
         return function () {
             /// These variables are created each time a Yoshi is made and only that Yoshi object can see them (so they never clash with other Yoshis).
                 
-            var facing = Math.round(Math.random()) || -1,
-                frame = 0,
+            var ai = true,
                 el = document.createElement("div"),
+                /// facing is either -1 for left or 1 for right
+                facing = Math.round(Math.random()) || -1,
+                frame = 0,
+                moving = false,
                 /// Create a random starting point
                 pos_x = Math.floor(Math.random() * 500),
                 pos_y = Math.floor(Math.random() * 140);
@@ -62,13 +65,22 @@ $(document).ready(function() {
             /**
              * Move the Yoshi
              *
-             * @param horizonal (int) Whether the Yoshi is moving right or left (-1 = left, 0 = neither, 1 = right)
-             * @param vertical  (int) Whether the Yoshi is moving up or down (-1 = up, 0 = neither, 1 = down)
+             * @param horizontal (int) Whether the Yoshi is moving right or left (-1 = left, 0 = neither, 1 = right)
+             * @param vertical   (int) Whether the Yoshi is moving up or down (-1 = up, 0 = neither, 1 = down)
              */
-            function walk(horizonal, vertical) {
+            function move(horizontal, vertical, running) {
+                if (moving) {
+                    return;
+                }
                 /// Make sure that the variables are numbers because they will be used in equations.
-                vertical = Number(horizonal);
-                vertical = Number(vertical);
+                horizontal = Number(horizontal);
+                vertical   = Number(vertical);
+                
+                /// AI might do this.
+                if (horizontal === 0 && vertical === 0) {
+                    stand();
+                    return;
+                }
                 
                 facing = horizontal || facing;
                 
@@ -79,18 +91,76 @@ $(document).ready(function() {
                 el.style.top  = pos_y;
                                 
                 set_frame();
+                moving = true;
+                
+                window.setTimeout(function () {
+                    moving = false;
+                }, (running ? 30 : 100));
             }
+            
+            function start_ai() {
+                var ai_horizontal = 0,
+                    ai_vertical   = 0,
+                    ai_running    = 0,
+                    
+                    ai_change_speed = Math.floor(Math.random() * 8) + 1,
+                    ai_timer;
+                
+                ai = true;
+                
+                function change_hor() {
+                    ai_horizontal = Math.floor(Math.random() * 3) - 1;
+                    if (ai) {
+                        window.setTimeout(change_hor, Math.floor(Math.random() * 700 * ai_change_speed) + (100 * ai_change_speed));
+                    }
+                }
+                
+                function change_vert() {
+                    ai_vertical = Math.floor(Math.random() * 3) - 1;
+                    if (ai) {
+                        window.setTimeout(change_vert, Math.floor(Math.random() * 700 * ai_change_speed) + (100 * ai_change_speed));
+                    }
+                }
+                
+                function change_running() {
+                    ai_running = Math.floor(Math.random() * 2);
+                    if (ai) {
+                        window.setTimeout(change_running, ai_running ? (Math.floor(Math.random() * 2000 * ai_change_speed) + (100 * ai_change_speed)) : (Math.floor(Math.random() * 50000 * ai_change_speed) + (6000 * ai_change_speed)));
+                    }
+                }
+                
+                change_hor();
+                change_vert();
+                change_running();
+                
+                ai_timer = window.setInterval(function () {
+                    if (!ai) {
+                        window.clearInterval(ai_timer1);
+                    }
+                    move(ai_horizontal, ai_vertical, ai_running);
+                }, 30);
+            }
+            
             
             /// Start the Yoshi standing.
             stand();
+            start_ai();
             /// Now that he is all ready, make him visible.
             document.body.appendChild(el);
             
             return {
-                walk: walk
+                take_control: function () {
+                    ai = false;
+                    stand();
+                },
+                relinquish_control: function () {
+                    start_ai();
+                },
+                move: move
             };
         };
     }());
+    
     
     Yoshi_maker();
     
